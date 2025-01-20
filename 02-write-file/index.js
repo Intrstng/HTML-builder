be48080c6ad994e40f0deb51e6dd1887ed6584f3
Intrstng <intrstg@gmail.com>
2025-01-20 12:40:40 +0300

    feat: complete task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,33 +3,38 @@ const path = require('path');
 const readline = require('readline');
 const { stdin, stdout } = process;
 
+const outputFilePath = path.join(__dirname, 'output.txt');
+
 const rl = readline.createInterface({
   input: stdin,
   output: stdout,
-  prompt: `\x1b[${33}m${`\nYou are currently in ${pathToHomeDirectory}\n`}\x1b[0m`
+  prompt: `\x1b[${34}m${`\nPlease, enter your text (type `}\x1b[${31}m${`exit`}\x1b[${34}m${` or press `}\x1b[${31}m${`Ctrl+C`}\x1b[${34}m${` to quit):\n`}\x1b[0m`
 });
 
-
-rl.on('line', async (input) => {
-  if (input === '.exit') {
-    this.#rl.emit('SIGINT');
-  };
-  try {
-    if (input) {
-      const parsedInputArgs = parseInputArguments(input);
-      this.#router(parsedInputArgs);
-    }
-  } catch (error) {
-    console.error(error.message);
-  }
-  // Set the prompt again for the next input
+const promptUser = () => {
   rl.prompt();
-});
 
-rl.on('SIGINT', () => {
-  const exitPhrase =  this.#colorize('Thank you for using File Manager, ', 31) +
-    this.#colorize(userName, 94) +
-    this.#colorize(' goodbye!', 31);
-  console.log(exitPhrase);
-  process.exit(0);
-});
+  rl.on('line', async (input) => {
+    if (input === 'exit') {
+      rl.emit('SIGINT');
+    }
+    try {
+      if (input) {
+        await fs.promises.appendFile(outputFilePath, input + '\n');
+        console.log(`\x1b[32mText added to \x1b[33m${outputFilePath}\x1b[0m`);
+      }
+    } catch (error) {
+      console.error(`\x1b[33mError writing to file: \x1b[31m${error.message}\x1b[0m`);
+    }
+    rl.prompt();
+  });
+
+  rl.on('SIGINT', () => {
+    const exitPhrase = `\x1b[31mYou finished executing of the app!\x1b[0m`;
+    console.log(exitPhrase);
+    rl.close();
+    process.exit(0);
+  });
+}
+
+promptUser();
